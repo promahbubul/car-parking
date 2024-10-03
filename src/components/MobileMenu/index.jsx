4a44f989be8eb7773ca4a87a6a9f23d3978d400d
promahbubul <mahbublalam500@gmail.com>
2024-10-03 12:35:35 +0600

    mobile responsive done

--- src/components/MobileMenu/index.jsx
+++ src/components/MobileMenu/index.jsx
@@ -22,18 +22,18 @@ const MobileMenu = () => {
   };
 
   return (
-    <div className="h-20  w-full bg-slate-700 fixed  bottom-0 justify-between items-center gap-[1px]  flex flex-row lg:hidden ">
+    <div className="h-14  w-full bg-slate-700 fixed  bottom-0 justify-between items-center gap-[1px]  flex flex-row lg:hidden ">
       {sidebarLinksData.map((item) => (
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? "md:px-5 md:py-3 text-sm sm:text-base font-medium  h-full justify-center  w-1/3 dark:text-black flex flex-col  items-center gap-1 bg-accent text-black"
+              ? "md:px-5 md:py-3 text-xs sm:text-base font-normal  h-full justify-center  w-1/3 dark:text-black flex flex-col  items-center gap-1 bg-accent text-black"
               : "md:px-5 md:py-3 text-sm sm:text-base  font-medium h-full justify-center items-center w-1/3 dark:text-slate-200 flex flex-col   gap-1 hover:bg-accent dark:bg-base-300 bg-gray-300 text-slate-800 hover:text-black"
           }
           key={item.id}
           to={item.to}
         >
-          <span className="text-xl">{item.icon()}</span>
+          <span className="text-lg">{item.icon()}</span>
           <span className="">{item.title}</span>
         </NavLink>
       ))}
